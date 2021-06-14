767574c1fed39f54fb534a6ce9ca6d7e43e87ac9
jttuboi <tuboi.jairo@gmail.com>
2021-06-13 22:47:25 -0300

    add list of words

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -10,17 +10,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Name Generator', home: RandomWords());
   }
 }
 
@@ -32,9 +22,37 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(title: const Text('Name Generator')),
+        body: _buildSuggestions());
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          // reajusta os indices, pois os Divider adicionado nas posições ímpares
+          // devem ser "ignorados" na hora de pegar um item da lista
+          final index = i ~/ 2;
+
+          // caso esteja no final da lista de sugestões, adiciona mais 10 palavras
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          // retorna a linha com a sugestão
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
   }
 }
