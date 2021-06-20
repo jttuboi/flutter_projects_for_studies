312d9e1616c6a4ce541ac5fb53987c09513b4638
jttuboi <tuboi.jairo@gmail.com>
2021-06-19 22:10:09 -0300

    add textfield and add task

--- app4/to_do_list/lib/main.dart
+++ app4/to_do_list/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/task.dart';
 
 void main() {
   runApp(App());
@@ -26,32 +27,70 @@ class Home extends StatelessWidget {
   }
 }
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = <Task>[];
+  TextEditingController textEditingController = TextEditingController();
+
+  void _addTask(String text) {
+    setState(() {
+      tasks.add(Task(text));
+    });
+    textEditingController.clear();
+  }
+
+  Widget _createTaskItem(Task task) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: Icon(
+            Icons.check_box,
+            size: 42.0,
+            color: Colors.green,
+          ),
+          padding: EdgeInsets.only(left: 10.0, right: 30.0),
+          onPressed: () {},
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.text,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(task.date.toIso8601String()),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: Icon(
-                Icons.check_box,
-                color: Colors.green,
-              ),
-              iconSize: 42.0,
-              onPressed: () {},
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("wash car"),
-                Text("01/01/2021"),
-              ],
-            ),
-          ],
+        Container(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            controller: textEditingController,
+            onSubmitted: _addTask,
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) => _createTaskItem(tasks[index]),
+          ),
         ),
       ],
     );
