3a3462c0fc24001c9433744c754bccd187451b05
jttuboi <tuboi.jairo@gmail.com>
2021-06-28 18:41:50 -0300

    basic

---
+++ app10/flutter_cookbook_animation_1/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Page1()));
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
