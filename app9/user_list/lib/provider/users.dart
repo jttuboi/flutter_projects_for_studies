0cee38633675802e69c7821d170431a56b227b85
jttuboi <tuboi.jairo@gmail.com>
2021-06-28 11:47:59 -0300

    add update and remove for users provider

--- app9/user_list/lib/provider/users.dart
+++ app9/user_list/lib/provider/users.dart
@@ -9,7 +9,7 @@ class Users with ChangeNotifier {
 
   List<User> get all {
     // ao duplicar o users, evita que algum lugar fora daqui tenha acesso
-    // a esses dados e possa alterar/remover
+    // a esses dados e possa alterar/remover a lista
     return [..._users.values];
   }
 
@@ -22,20 +22,40 @@ class Users with ChangeNotifier {
   }
 
   void put(User user) {
-    final id = Random().nextDouble().toString();
-    _users.putIfAbsent(
-      id,
-      () => User(
-        id: id,
-        name: user.name,
-        email: user.email,
-        avatarUrl: user.avatarUrl,
-      ),
-    );
+    if (user.id.trim().isNotEmpty && _users.containsKey(user.id)) {
+      // update User
+      _users.update(
+          user.id,
+          (_) => User(
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                avatarUrl: user.avatarUrl,
+              ));
+    } else {
+      // add new User
+      final id = Random().nextDouble().toString();
+      _users.putIfAbsent(
+        id,
+        () => User(
+          id: id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
+    }
 
     // necessita chamar esse método para notificar que os dados foram alterados
     // o provider que utiliza desse ChangeNotifier irá fazer suas devidas atualizações
     // dependendo da sua implementação fora daqui
     notifyListeners();
+  }
+
+  remove(User user) {
+    if (user.id.trim().isNotEmpty) {
+      _users.remove(user.id);
+      notifyListeners();
+    }
   }
 }
