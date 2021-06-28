7776a2b7399eed5bc23519f332609e4985c43060
jttuboi <tuboi.jairo@gmail.com>
2021-06-28 14:42:24 -0300

    convert user form for stateful. refactoring.

--- app9/user_list/lib/views/user_form.dart
+++ app9/user_list/lib/views/user_form.dart
@@ -3,9 +3,25 @@ import 'package:provider/provider.dart';
 import 'package:user_list/models/user.dart';
 import 'package:user_list/provider/users.dart';
 
-class UserForm extends StatelessWidget {
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
   final _form = GlobalKey<FormState>();
+
   final Map<String, String> _formData = {"id": ""};
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final Object? user = ModalRoute.of(context)!.settings.arguments;
+    if (user != null) {
+      _loadFormData(user as User);
+    }
+  }
 
   _loadFormData(User user) {
     _formData["id"] = user.id;
@@ -14,33 +30,53 @@ class UserForm extends StatelessWidget {
     _formData["avatarUrl"] = user.avatarUrl;
   }
 
+  String? _nameValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "name can't be empty";
+    }
+    if (value.trim().length < 3) {
+      return "name is too small";
+    }
+    return null;
+  }
+
+  String? _emailValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "e-mail can't be empty";
+    }
+    return null;
+  }
+
+  String? _urlValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "url can't be empty";
+    }
+    return null;
+  }
+
+  _onSave() {
+    bool isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+      Provider.of<Users>(context, listen: false).put(User(
+        id: _formData["id"]!,
+        name: _formData["name"]!,
+        email: _formData["email"]!,
+        avatarUrl: _formData["avatarUrl"]!,
+      ));
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Object? user = ModalRoute.of(context)!.settings.arguments;
-
-    if (user != null) {
-      _loadFormData(user as User);
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("User Form"),
         actions: [
           IconButton(
             icon: Icon(Icons.save),
-            onPressed: () {
-              bool isValid = _form.currentState!.validate();
-              if (isValid) {
-                _form.currentState!.save();
-                Provider.of<Users>(context, listen: false).put(User(
-                  id: _formData["id"]!,
-                  name: _formData["name"]!,
-                  email: _formData["email"]!,
-                  avatarUrl: _formData["avatarUrl"]!,
-                ));
-                Navigator.of(context).pop();
-              }
-            },
+            onPressed: _onSave,
           ),
         ],
       ),
@@ -53,39 +89,21 @@ class UserForm extends StatelessWidget {
               TextFormField(
                 initialValue: _formData["name"],
                 keyboardType: TextInputType.name,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "name can't be empty";
-                  }
-                  if (value.trim().length < 3) {
-                    return "name is too small";
-                  }
-                  return null;
-                },
+                validator: _nameValidator,
                 onSaved: (value) => _formData["name"] = value!,
                 decoration: InputDecoration(labelText: "Name"),
               ),
               TextFormField(
                 initialValue: _formData["email"],
                 keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "e-mail can't be empty";
-                  }
-                  return null;
-                },
+                validator: _emailValidator,
                 onSaved: (value) => _formData["email"] = value!,
                 decoration: InputDecoration(labelText: "E-mail"),
               ),
               TextFormField(
                 initialValue: _formData["avatarUrl"],
                 keyboardType: TextInputType.url,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "url can't be empty";
-                  }
-                  return null;
-                },
+                validator: _urlValidator,
                 onSaved: (value) => _formData["avatarUrl"] = value!,
                 decoration: InputDecoration(labelText: "Avatar URL"),
               ),
