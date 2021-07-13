2415870b9227370c4eb36091cf57c7947a1d18a2
jttuboi <tuboi.jairo@gmail.com>
2021-07-13 14:27:41 -0300

    update home page to list

--- app10/flutter_cookbook/lib/main.dart
+++ app10/flutter_cookbook/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:flutter_cookbook/design_6.dart';
 import 'package:flutter_cookbook/effect_1.dart';
 import 'package:flutter_cookbook/effect_2.dart';
 import 'package:flutter_cookbook/effect_3.dart';
+import 'package:flutter_cookbook/effect_4.dart';
 
 class Routes {
   static const String home = "/";
@@ -28,6 +29,7 @@ class Routes {
   static const String effect_1 = "/effect_1";
   static const String effect_2 = "/effect_2";
   static const String effect_3 = "/effect_3";
+  static const String effect_4 = "/effect_4";
 }
 
 void main() => runApp(
@@ -47,6 +49,7 @@ void main() => runApp(
           Routes.effect_1: (context) => Effect1(),
           Routes.effect_2: (context) => Effect2(),
           Routes.effect_3: (context) => Effect3(),
+          Routes.effect_4: (context) => Effect4(),
         },
         debugShowCheckedModeBanner: false,
         // design 5
@@ -77,7 +80,6 @@ void main() => runApp(
           } else {
             throw Exception('Unknown route: ${settings.name}');
           }
-
           return MaterialPageRoute<dynamic>(
             builder: (context) {
               return page;
@@ -88,124 +90,113 @@ void main() => runApp(
       ),
     );
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List<Detail> _details;
+
+  @override
+  void initState() {
+    _details = [
+      // é testado animações diferenciadas para mudanças de tela.
+      // obs: as animações funcionam com qualquer widget que aceite animações.
+      // https://flutter.dev/docs/cookbook/animation/page-route-animation.html
+      Detail(title: "animation - change screen", route: Routes.animation_1),
+
+      // é testado animações arrastar e soltar em um widget com imagem.
+      // o efeito da animação utilizada é mais complexa, usando controller
+      // e SpringSimulation.
+      // https://flutter.dev/docs/cookbook/animation/physics-simulation.html
+      Detail(
+          title: "animation - drag/drop container", route: Routes.animation_2),
+
+      // é testado animações do container.
+      // tem o widget AnimationContainer e é mostrado parte de seu uso
+      // voltado para animação, já que a outra parte é o container em si
+      // https://flutter.dev/docs/cookbook/animation/animated-container.html
+      Detail(
+          title: "animation - animation container", route: Routes.animation_3),
+
+      // é testado animação de aparecer e desaparecer com widget AnimatedOpacity.
+      // https://flutter.dev/docs/cookbook/animation/opacity-animation.html
+      Detail(
+          title: "animation - animation fade in/out",
+          route: Routes.animation_4),
+
+      // uso do Drawer.
+      // https://flutter.dev/docs/cookbook/design/drawer.html
+      Detail(title: "design - drawer", route: Routes.design_1),
+
+      // uso do SnackBar.
+      // como se fosse a notificação, porém dentro do app e é mostrado na parte inferior
+      // https://flutter.dev/docs/cookbook/design/snackbars.html
+      Detail(title: "design - snackbar", route: Routes.design_2),
+
+      // uso de fonts diferenciadas.
+      // tem a forma padrão via projeto e a forma via google_fonts
+      // https://pub.dev/packages/google_fonts
+      // https://flutter.dev/docs/cookbook/design/fonts.html
+      // https://flutter.dev/docs/cookbook/design/package-fonts.html (this is not made it)
+      Detail(title: "design - fonts", route: Routes.design_3),
+
+      // uso de orientação
+      // para celular, tem que girar o celular
+      // para desktop e web, tem que mudar o tamanho da tela
+      // https://flutter.dev/docs/cookbook/design/orientation.html
+      Detail(title: "design - orientation", route: Routes.design_4),
+
+      // uso de themes
+      // olhar no topo que tem o theme default descrito no MaterialApp
+      // https://flutter.dev/docs/cookbook/design/themes
+      Detail(title: "design - theme", route: Routes.design_5),
+
+      // uso de tabs
+      // https://flutter.dev/docs/cookbook/design/tabs
+      Detail(title: "design - tabs", route: Routes.design_6),
+
+      // download effect
+      // https://flutter.dev/docs/cookbook/effects/download-button
+      Detail(title: "effect - download", route: Routes.effect_1),
+
+      // controle de fluxo
+      // um controle alternativo das routes, onde pode concentrar o controle
+      // do fluxo.
+      // https://flutter.dev/docs/cookbook/effects/nested-nav
+      Detail(title: "effect - nested navigation flow", route: Routes.effect_2),
+
+      // adiciona filtro nas fotos utilizando scroll dos filtros
+      // utiliza o PageView para fazer o efeito do carousel, onde troca a
+      // cor dos filtros pra foto
+      // https://flutter.dev/docs/cookbook/effects/photo-filter-carousel
+      Detail(title: "effect - photo filter carousel", route: Routes.effect_3),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            // é testado animações diferenciadas para mudanças de tela.
-            // obs: as animações funcionam com qualquer widget que aceite animações.
-            // https://flutter.dev/docs/cookbook/animation/page-route-animation.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.animation_1),
-              child: Text("animation - change screen"),
-            ),
-
-            // é testado animações arrastar e soltar em um widget com imagem.
-            // o efeito da animação utilizada é mais complexa, usando controller
-            // e SpringSimulation.
-            // https://flutter.dev/docs/cookbook/animation/physics-simulation.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.animation_2),
-              child: Text("animation - drag/drop container"),
-            ),
-
-            // é testado animações do container.
-            // tem o widget AnimationContainer e é mostrado parte de seu uso
-            // voltado para animação, já que a outra parte é o container em si
-            // https://flutter.dev/docs/cookbook/animation/animated-container.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.animation_3),
-              child: Text("animation - animation container"),
-            ),
-
-            // é testado animação de aparecer e desaparecer com widget AnimatedOpacity.
-            // https://flutter.dev/docs/cookbook/animation/opacity-animation.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.animation_4),
-              child: Text("animation - animation fade in/out"),
-            ),
-
-            // uso do Drawer.
-            // https://flutter.dev/docs/cookbook/design/drawer.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_1),
-              child: Text("design - drawer"),
-            ),
-
-            // uso do SnackBar.
-            // como se fosse a notificação, porém dentro do app e é mostrado na parte inferior
-            // https://flutter.dev/docs/cookbook/design/snackbars.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_2),
-              child: Text("design - snackbar"),
-            ),
-
-            // uso de fonts diferenciadas.
-            // tem a forma padrão via projeto e a forma via google_fonts
-            // https://pub.dev/packages/google_fonts
-            // https://flutter.dev/docs/cookbook/design/fonts.html
-            // https://flutter.dev/docs/cookbook/design/package-fonts.html (this is not made it)
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_3),
-              child: Text("design - fonts"),
-            ),
-
-            // uso de orientação
-            // para celular, tem que girar o celular
-            // para desktop e web, tem que mudar o tamanho da tela
-            // https://flutter.dev/docs/cookbook/design/orientation.html
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_4),
-              child: Text("design - orientation"),
-            ),
-
-            // uso de themes
-            // olhar no topo que tem o theme default descrito no MaterialApp
-            // https://flutter.dev/docs/cookbook/design/themes
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_5),
-              child: Text("design - theme"),
-            ),
-
-            // uso de tabs
-            // https://flutter.dev/docs/cookbook/design/tabs
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.design_6),
-              child: Text("design - tabs"),
-            ),
-
-            // download effect
-            // https://flutter.dev/docs/cookbook/effects/download-button
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.effect_1),
-              child: Text("effect - download"),
-            ),
-
-            // nested navigation flow
-            // um controle alternativo das routes, onde pode concentrar o controle
-            // do fluxo.
-            // https://flutter.dev/docs/cookbook/effects/nested-nav
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.effect_2),
-              child: Text("effect - nested navigation flow"),
-            ),
-
-            // photo filter carousel
-            // utiliza o PageView para fazer o efeito do carousel, onde troca a
-            // cor dos filtros pra foto
-            // https://flutter.dev/docs/cookbook/effects/photo-filter-carousel
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, Routes.effect_3),
-              child: Text("effect - photo filter carousel"),
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: _details.length,
+        itemBuilder: (context, index) {
+          return ElevatedButton(
+            onPressed: () =>
+                Navigator.pushNamed(context, _details[index].route),
+            child: Text(_details[index].title),
+          );
+        },
       ),
     );
   }
+}
+
+class Detail {
+  String title;
+  String route;
+
+  Detail({required this.title, required this.route});
 }
