909dde7ce7ce87410d3de83699dcf99b05f77369
jttuboi <tuboi.jairo@gmail.com>
2021-07-20 00:47:26 -0300

    add service

--- app12/semana_do_flutter_arquitetura/lib/app/pages/app_controller.dart
+++ app12/semana_do_flutter_arquitetura/lib/app/pages/app_controller.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/foundation.dart';
+import 'package:semana_do_flutter_arquitetura/app/interfaces/local_storage_interface.dart';
 import 'package:semana_do_flutter_arquitetura/app/models/appconfig_model.dart';
+import 'package:semana_do_flutter_arquitetura/app/services/shared_local_storage_service.dart';
 
 class AppController {
   // construtor privado
-  AppController._();
+  AppController._() {
+    storage.get("isDark").then((value) {
+      themeSwitch.value = (value != null) ? value : false;
+    });
+  }
 
   // transformando essa classe em singleton
   static final AppController instance = AppController._();
@@ -14,9 +20,11 @@ class AppController {
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
   final AppConfigModel config = AppConfigModel();
+  final ILocalStorage storage = SharedLocalStorageService();
 
   changeTheme(bool value) {
     // no momento que essa variavel for alterada, ela atualizará quem utiliza essa variavel
-    config.themeSwitch.value = value;
+    themeSwitch.value = value;
+    storage.put("isDark", value);
   }
 }
