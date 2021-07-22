2a63ca4f59a38fa1c2b9ced131a75101a6c785c8
jttuboi <tuboi.jairo@gmail.com>
2021-07-22 18:13:35 -0300

    init

---
+++ app19/page_view_dynamically/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
