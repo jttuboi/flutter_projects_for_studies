47c5233bfef285c478f47456f5917378ae22d4b9
jttuboi <tuboi.jairo@gmail.com>
2021-07-25 15:03:26 -0300

    init cards

---
+++ app21/cards/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    ),
+  ));
+}
