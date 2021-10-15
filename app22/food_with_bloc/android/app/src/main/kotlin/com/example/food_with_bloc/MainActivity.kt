e4a2e28c51b5975c9b6617c84bf12510e33932f9
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2021-10-15 16:00:34 -0300

    add some missing files after update gitignore

---
+++ app22/food_with_bloc/android/app/src/main/kotlin/com/example/food_with_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_with_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app23/counter_with_bloc/android/app/src/main/kotlin/com/example/counter_with_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.counter_with_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
