306a29c66ced30462eef8b5fe2f64a8a48779085
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2021-10-22 12:13:53 -0300

    add init form validation project

---
+++ app25/form_validation/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
