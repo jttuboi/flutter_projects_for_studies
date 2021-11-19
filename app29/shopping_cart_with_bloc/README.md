bc5aa5e45fba4d9e13a3a7ca52b329abca32d340
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2021-11-19 18:42:23 -0300

    add shopping cart project

---
+++ app29/shopping_cart_with_bloc/README.md
@@ -0,0 +1,3 @@
+# shopping_cart_with_bloc
+
+https://github.com/felangel/bloc/tree/master/examples/flutter_shopping_cart
---
+++ app29/shopping_cart_with_bloc/android/app/src/main/kotlin/com/example/shopping_cart_with_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopping_cart_with_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
