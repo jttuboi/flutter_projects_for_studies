8f90a8318fac562353c1e0f3d73519c9cf15469d
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2022-01-10 09:45:14 -0300

    added navigator 2.0 tutorial 1 project

---
+++ app32/navigator_2_0_tut_1/android/app/src/main/kotlin/com/example/navigator_2_0_tut_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigator_2_0_tut_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app32/navigator_2_0_tut_1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
