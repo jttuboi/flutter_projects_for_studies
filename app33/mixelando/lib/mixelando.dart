1ce82f891898a5ba74c0f8dc9e08fc6312516c31
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2022-01-11 12:32:02 -0300

    add links

--- app33/mixelando/lib/mixelando.dart
+++ app33/mixelando/lib/mixelando.dart
@@ -1,6 +1,9 @@
 import 'package:mixelando/birds/dove.dart';
 import 'package:mixelando/mammals/cat.dart';
 
+// https://medium.com/flutter-community/dart-what-are-mixins-3a72344011f3
+// https://www.digitalocean.com/community/tutorials/dart-mixins
+
 void main(List<String> arguments) {
   Dove dove = Dove('cagão');
   dove.fly();
