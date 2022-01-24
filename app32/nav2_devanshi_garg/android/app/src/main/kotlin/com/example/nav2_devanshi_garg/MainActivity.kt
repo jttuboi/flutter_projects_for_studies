6a34eca11f3ad8de30093d65976e6120bfd9860c
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2022-01-24 01:18:11 -0300

    init tutorial navigation 1

---
+++ app32/nav2_devanshi_garg/android/app/src/main/kotlin/com/example/nav2_devanshi_garg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nav2_devanshi_garg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app32/nav2_devanshi_garg/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
