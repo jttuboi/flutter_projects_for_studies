677d979335b657307f886e481547868433d18a8b
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2022-01-26 01:04:48 -0300

    add tutorial lulupointu for navigation 2.0

---
+++ app32/nav2_lulupointu/android/app/src/main/kotlin/com/example/nav2_lulupointu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nav2_lulupointu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app32/nav2_lulupointu/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
