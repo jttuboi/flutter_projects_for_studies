33ea1c1af8b4cdc88af8ebf681023fa309971bb6
Jairo Toshio Tuboi <tuboi.jairo@gmail.com>
2022-02-01 00:57:35 -0300

    create authentication params

--- app36/manguinho/lib/domain/usecases/authentication.dart
+++ app36/manguinho/lib/domain/usecases/authentication.dart
@@ -1,5 +1,15 @@
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({required String email, required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  AuthenticationParams({
+    required this.email,
+    required this.secret,
+  });
+
+  final String email;
+  final String secret;
 }
