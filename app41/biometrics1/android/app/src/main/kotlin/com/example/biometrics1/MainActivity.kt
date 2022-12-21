bea68591403d7130edddf19a397d2bfe238b1660
Jairo Tuboi <tuboi.jairo@gmail.com>
2022-12-21 14:51:35 -0300

    init biometrics

---
+++ app41/biometrics1/android/app/src/main/kotlin/com/example/biometrics1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.biometrics1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app41/biometrics1/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
