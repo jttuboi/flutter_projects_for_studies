f1a5f88669cd657d787f65aa2e3c1eedb507089d
Jairo Tuboi <tuboi.jairo@gmail.com>
2024-07-16 12:47:53 -0300

    add chat example

---
+++ 049_chat/README.md
@@ -0,0 +1,3 @@
+# chat
+
+https://www.youtube.com/watch?v=eYFhH7Gf5mI&list=PLFhJomvoCKC_mXNBDnpO46Hea_PkCiVWS&index=2
---
+++ 049_chat/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
